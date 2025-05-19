8b2e5ff89f4ab858f1124709186c28e50f2475aa
sandeep <sandeeppatil10705@gmail.com>
2025-05-19 20:03:11 +0530

    Make add to cart interactive

--- scripts/index.js
+++ scripts/index.js
@@ -19,7 +19,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${ (product.priceCents / 100).toFixed(2) }
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -44,11 +44,31 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart " data-product-id=" ${product.id}">
             Add to Cart
           </button>
         </div>
         `;
 })
 
-document.querySelector('.js-products-grid').innerHTML=productsHTML;
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+        let matchingItem;
+        cart.forEach((item)=>{
+            if(productId===item.productId)matchingItem=item;
+        })
+        if(matchingItem)matchingItem.quantity+=1;
+        else{cart.push({
+
+            productId,
+            quantity: 1,
+        })}
+        
+        console.log(cart);
+
+    })
+
+});
