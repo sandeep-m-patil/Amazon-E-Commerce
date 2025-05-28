fb61c7589c8959b1b7ffd9b816b4503c8068c486
sandeep <sandeeppatil10705@gmail.com>
2025-05-28 21:06:54 +0530

    Use in checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,6 @@
 import { renderOderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js'
+
 renderOderSummary(); 
 renderPaymentSummary(); 
